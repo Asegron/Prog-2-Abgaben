3017f7bf87593279655266dab4b72ae05d398b71
Asegron <58540933+Asegron@users.noreply.github.com>
2022-05-16 21:03:30 +0200

    Add files via upload

---
+++ ueb16/Utils.java
@@ -0,0 +1,14 @@
+public class Utils
+{
+    /**
+     * Check Methode, wirft IllegalArgumentException, 
+     * falls eine Bedingung nicht erfüllt ist.
+     * @param Bedingung 
+     * @param msg
+     */
+    public static void check(boolean bedingung, String msg) {
+        if(bedingung)  {
+            throw new IllegalArgumentException(msg);
+        }
+    }
+}
