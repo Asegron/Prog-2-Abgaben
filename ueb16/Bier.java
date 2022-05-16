2d700cc2ee1329ca04d6fd6ee25610450d5e6631
Asegron <58540933+Asegron@users.noreply.github.com>
2022-05-16 22:05:26 +0200

    Add files via upload

---
+++ ueb16/Bier.java
@@ -0,0 +1,49 @@
+public class Bier extends AlkoholischesGetraenk {
+    private String brauerei;
+    private String regexName = "^[a-zA-Z]+$";
+
+    public Bier(String name, float alkoholgehalt, String brauerei) {
+        super(name,alkoholgehalt);
+
+        setBrauerei(brauerei);
+    }
+
+    public String getBrauerei() {
+        return brauerei;
+    }
+
+    public void setBrauerei(String brauerei) {
+        Utils.check(brauerei == null || brauerei.trim().isEmpty(),
+         "Es muss ein Name für die Brauerei eingegeben werden!");
+        Utils.check(brauerei.matches(regexName) == false,
+            "Der Name der Braurei darf nur Buchstaben enthalten!");
+            this.brauerei = brauerei;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + brauerei;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (!(obj instanceof Bier)) {
+            return false;
+        }
+        Bier other = (Bier) obj;
+        if (brauerei == null) {
+            if (other.brauerei != null) {
+                return false;
+            }
+        } else if (!brauerei.equals(other.brauerei)) {
+            return false;
+        }
+        return true;
+    }
+}
