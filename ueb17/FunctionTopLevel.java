bb87024bdb4ff0fc5836f7b8c2c66d29d2fc08c5
DesiB832 <57222672+DesiB832@users.noreply.github.com>
2022-05-27 18:40:36 +0200

    Add files via upload

---
+++ ueb17/FunctionTopLevel.java
@@ -0,0 +1,40 @@
+
+/**
+ * Klasse FunctionsTopLevel implementiert die Funktion f(x) = x! als TopLevel-Klasse.
+ * 
+ * @author Desiree Becker, Daniel Stelz
+ * @version 27.05.22
+ */
+public class FunctionTopLevel{
+    private static final int BEGINN = 1;
+    private static final int END    = 10;
+    
+    private static final String MSG_FAKULTAET = "f(x) = x! : ";
+    
+    MethodeApplyAndPrint m;
+    private FunctionTopLevel(){
+        m = new MethodeApplyAndPrint();
+    }
+    
+     /**
+     * Methode start, die die applyAndPrint mit der Fakultaet aufruft
+     *
+     */
+    public void start(){
+        Fakultaet ff = new Fakultaet();
+        System.out.println(MSG_FAKULTAET);
+        m.applyAndPrint(BEGINN, END, ff);
+        
+        
+    }
+    
+    /**
+     * Methode main zum Starten des Programms.
+     *
+     */
+    public static void main(String[] args){
+        FunctionTopLevel f = new FunctionTopLevel();
+        f.start();
+    
+    }
+}
